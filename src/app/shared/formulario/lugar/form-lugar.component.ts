e1755b8bbe6d0385e963aa3c77cac769ee02d934
areangonzalez <areangonzalez@gmail.com>
2019-02-09 23:01:27 -0300

    TG-74 #Closed listado de localidades en el formulario de lugar

--- src/app/shared/formulario/lugar/form-lugar.component.ts
+++ src/app/shared/formulario/lugar/form-lugar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from "@angular/forms";
+import { MensajesService, LocalidadService } from 'src/app/core/services';
 
 @Component({
   selector: 'shared-form-lugar',
@@ -10,14 +11,25 @@ export class FormLugarComponent implements OnInit {
   @Input('formLugar') public lugar: FormGroup;
 
   public submitted: boolean = false;
-  public localidadLista: any = [{id: 1, nombre: 'Roca'}, {id: 2, nombre: 'Viedma'}];
+  public localidadLista: any = [];
 
-  constructor(){}
+  constructor(private _mensajeService: MensajesService, private _localidadService: LocalidadService){}
 
   ngOnInit() {
+    this.listarLocalidades();
   }
 
   // convenience getter for easy access to form fields
   get datosLugar() { return this.lugar.controls; }
+
+
+  /* Funcionalidad para los listados del formulario */
+
+  public listarLocalidades() {
+    this._localidadService.listar().subscribe(
+      datos => {
+        this.localidadLista = datos;
+      }, error => { this._mensajeService.cancelado(error, [{name:''}]); });
+  }
 
 }
