c398b4a29a97f290fa42947aa79c7c3deba35465
areangonzalez <areangonzalez@gmail.com>
2019-02-25 14:28:56 -0300

    TG-125 configuracion del tab de prestacion

--- src/app/reporte/busqueda/busqueda-recurso.component.ts
+++ src/app/reporte/busqueda/busqueda-recurso.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LocalidadService, ProgramaService, TipoRecursoService, MensajesService } from 'src/app/core/services';
+import { UtilService } from 'src/app/core/utils';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'busqueda-beneficiario',
@@ -6,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busqueda-beneficiario.component.sass']
 })
 export class BusquedaRecursoComponent implements OnInit {
+  @Input("busquedaAvanzada") public busquedaAvanzada: FormGroup;
 
-  constructor(){}
+  public localidadesLista: any[] = [];
+  public programasLista: any[] = [];
+  public tipoRecursosLista: any[] =[];
+  public globalSearch:string = '';
+  public mostrar: boolean = false;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _localidadService: LocalidadService,
+    private _programaService: ProgramaService,
+    private _tipoRecursoService: TipoRecursoService,
+    private _mensajeService: MensajesService,
+    private _util: UtilService
+  ){
+
+    // creacion de fomulario para busqueda avanzada
+    this.busquedaAvanzada = _fb.group({
+      localidadid: '',
+      programaid: '',
+      tipo_recursoid: '',
+      fecha_desde: '',
+      fechaDesde: '',
+      fechaHasta: '',
+      fecha_hasta: ''
+    });
+  }
 
   ngOnInit() {
+    this.listarLocalidades();
+    this.listarProgramas();
+    this.listarTipoRecursos(5);
   }
+
+  public listarLocalidades(){
+    this._localidadService.listar().subscribe(
+      localidades => { this.localidadesLista = localidades; },
+      error => { this._mensajeService.cancelado(error, [{name:''}]); });
+  }
+
+  public listarProgramas(){
+    this._programaService.listar().subscribe(
+      programas => { this.programasLista = programas; },
+      error => { this._mensajeService.cancelado(error, [{name:''}]); });
+  }
+
+  public listarTipoRecursos(programaid:number){
+    this._tipoRecursoService.buscarPorPrograma(programaid).subscribe(
+      tipoRecursos => { this.tipoRecursosLista = tipoRecursos; },
+      error => { this._mensajeService.cancelado(error, [{name:''}]); });
+  }
+
+  public formatFechaDesde(obj:any){
+    this.busquedaAvanzada.controls.fecha_desde.setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
+  }
+
+  public formatFechaHasta(obj:any){
+    this.busquedaAvanzada.controls.fecha_hasta.setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
+  }
+
+  public buscar(busqueda:string, page:number) {
+    let busquedaAvanzada = this.busquedaAvanzada.value;
+    let apiBusqueda:object = {page:page, pagesize:20, global_param: busqueda};
+    for (const clave in busquedaAvanzada) {
+      if(busquedaAvanzada[clave] !== '') {
+        apiBusqueda[clave] = busquedaAvanzada[clave];
+      }
+    }
+    //this.listarRecursos(apiBusqueda);
+  }
+
+  public mostrarBusquedaAvanzada(){
+    return this.mostrar = !this.mostrar;
+  }
+
 }
--- src/app/reporte/reporte.component.ts
+++ src/app/reporte/reporte.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
 import { LocalidadService, ProgramaService, TipoRecursoService, MensajesService, RecursoSocialService } from '../core/services';
 import { UtilService } from '../core/utils';
@@ -11,20 +10,10 @@ import { UtilService } from '../core/utils';
   providers: [NgbTabsetConfig]
 })
 export class ReporteComponent implements OnInit {
-  public globalSearch:string = '';
-  public mostrar:boolean = false;
-  public busquedaAvanzada: FormGroup;
-  public localidadesLista: any[] = [];
-  public programasLista: any[] = [];
-  public tipoRecursosLista: any[] =[];
   public recursosLista: any[] = [];
   public configPaginacion:any = { "colleccionSize": 0, "pageSize": 0, "page": 1 };
 
   constructor(
-    private _fb: FormBuilder,
-    private _localidadService: LocalidadService,
-    private _programaService: ProgramaService,
-    private _tipoRecursoService: TipoRecursoService,
     private _mensajeService: MensajesService,
     private _util: UtilService,
     private _recursoService: RecursoSocialService,
@@ -32,25 +21,12 @@ export class ReporteComponent implements OnInit {
   ){
     _configTabSet.justify = 'center';
     _configTabSet.type = 'pills';
-    // creacion de fomulario para busqueda avanzada
-    this.busquedaAvanzada = _fb.group({
-      localidadid: '',
-      programaid: '',
-      tipo_recursoid: '',
-      fecha_desde: '',
-      fechaDesde: '',
-      fechaHasta: '',
-      fecha_hasta: ''
-    });
   }
 
   ngOnInit() {
-    this.listarLocalidades();
-    this.listarProgramas();
-    this.listarTipoRecursos(5);
+
     this.buscar(' ', 1);
   }
-
 
   public buscar(busqueda:string, page:number) {
     let busquedaAvanzada = this.busquedaAvanzada.value;
@@ -60,40 +36,10 @@ export class ReporteComponent implements OnInit {
         apiBusqueda[clave] = busquedaAvanzada[clave];
       }
     }
-
     this.listarRecursos(apiBusqueda);
-
   }
 
-  public mostrarBusquedaAvanzada(){
-    return this.mostrar = !this.mostrar;
-  }
 
-  public listarLocalidades(){
-    this._localidadService.listar().subscribe(
-      localidades => { this.localidadesLista = localidades; },
-      error => { this._mensajeService.cancelado(error, [{name:''}]); });
-  }
-
-  public listarProgramas(){
-    this._programaService.listar().subscribe(
-      programas => { this.programasLista = programas; },
-      error => { this._mensajeService.cancelado(error, [{name:''}]); });
-  }
-
-  public listarTipoRecursos(programaid:number){
-    this._tipoRecursoService.buscarPorPrograma(programaid).subscribe(
-      tipoRecursos => { this.tipoRecursosLista = tipoRecursos; },
-      error => { this._mensajeService.cancelado(error, [{name:''}]); });
-  }
-
-  public formatFechaDesde(obj:any){
-    this.busquedaAvanzada.controls.fecha_desde.setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
-  }
-
-  public formatFechaHasta(obj:any){
-    this.busquedaAvanzada.controls.fecha_hasta.setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
-  }
 
   public listarRecursos(params:object){
     this._recursoService.buscar(params).subscribe(
@@ -106,6 +52,7 @@ export class ReporteComponent implements OnInit {
   }
 
   public cambioPagina(page){
-    this.buscar(this.globalSearch, page);
+    //this.buscar(this.globalSearch, page);
+    this.buscar(' ', page);
   }
 }
