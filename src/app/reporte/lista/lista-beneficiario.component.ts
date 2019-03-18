0d206e9cc442d24c46c4ac7cb2136fe40df6c753
areangonzalez <areangonzalez@gmail.com>
2019-03-18 13:17:31 -0300

    TG-207 #Closed se crea el listado de beneficiario

--- src/app/reporte/lista/lista-beneficiario.component.ts
+++ src/app/reporte/lista/lista-beneficiario.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DatePipe } from "@angular/common";
 
 @Component({
   selector: 'lista-beneficiario',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-beneficiario.component.sass']
 })
 export class ListaBeneficiarioComponent implements OnInit {
+  @Input("beneficiariosLista") public beneficiariosLista:any;
+  @Input("configPaginacion") public configPaginacion:any;
+  @Output("cambioDePagina") public cambioDePagina = new EventEmitter();
 
   constructor(){}
 
   ngOnInit() {
+  }
+
+  cambioPagina(page){
+    this.cambioDePagina.emit(page);
+  }
+
+  public actualizarLista(estado:any){
+    if (estado){
+      this.cambioDePagina.emit(this.configPaginacion.page);
+    }
   }
 
 }
