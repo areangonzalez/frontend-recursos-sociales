3f2462131d515813b358707a23dde9573e666254
areangonzalez <areangonzalez@gmail.com>
2019-03-29 10:03:15 -0300

    TG-238 #Closed se configura el grafico de beneficiarios por programa

--- src/app/reporte/chart/chart-programa-beneficiarios.component.ts
+++ src/app/reporte/chart/chart-programa-beneficiarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { MensajesService, ProgramaService } from 'src/app/core/services';
 
 @Component({
   selector: 'reporte-programa-beneficiarios',
@@ -8,28 +9,52 @@ import { Chart } from 'chart.js';
 export class ChartProgramaBeneficiariosComponent implements OnInit {
 
   public chart:any;
-  public pieChartLabels = ['Emprender', 'Habitat', 'Micro Emprenimientos', 'Río negro presente', 'subsidio'];
-  public pieChartData = [120, 150, 180, 90, 30];
-  public pieChartType = 'pie';
+  public colorsGrafico: any[] = ['red', 'orange', 'yellow', 'green', 'blue'];
+  public datosPrograma: any[] = [];
 
-  constructor(){}
+  constructor(
+    private _mensajeService: MensajesService,
+    private _programaService: ProgramaService
+  ){
+
+  }
 
   ngOnInit() {
+    this.mostrarGrafico();
+    this.obtenerDatosPrograma();
+  }
+
+  private obtenerDatosPrograma(){
+    this._programaService.listar()
+    .subscribe(programa => {
+      programa.forEach((val, i) => {
+        this.datosPrograma.push({ nombre: programa[i].nombre, color: this.colorsGrafico[i] });
+        // nombre de programas
+        this.chart.data.labels.push(programa[i].nombre);
+        // cantidad personas
+        this.chart.data.datasets[0].data.push(programa[i].persona_cantidad);
+        this.chart.update();
+      });
+    }, error => { this._mensajeService.cancelado(error, [{name:''}]); });
+  }
+
+  private mostrarGrafico(){
     this.chart = new Chart('programa-beneficiarios', {
       type: 'pie',
       data: {
-        labels: this.pieChartLabels,
+        labels: [],
         datasets: [
           {
-            label: 'Beneficiarios',
-            data: [45, 50, 20, 5, 180],
-            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+            label: 'Prestaciones',
+            data: [],
+            backgroundColor: this.colorsGrafico,
             fill: false
           }
-        ]      },
+        ]
+      },
       options: {
         legend: {
-          display: true
+          display: false
         }
       }
     });
