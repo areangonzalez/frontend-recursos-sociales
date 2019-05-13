0326ba9d0a214a4385ffebef0181e47be75355c9
areangonzalez <areangonzalez@gmail.com>
2019-05-13 13:04:15 -0300

    arreglo en login

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { environment } from "../../../environments/environment";
 import { ApiService } from './api.service';
 import { JwtService } from './jwt.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
-    url: string = environment.baseUrl + '/usuarios/login';
     constructor(
       private _apiService: ApiService,
       private _jwtService: JwtService,
     ) { }
 
     login(params) {
-        //return this.http.post<any>(this.url, { username: params.username, password_hash: params.password })
-        return this._apiService.post(this.url, { username: params.username, password_hash: params.password })
+        return this._apiService.post('/usuarios/login', { username: params.username, password_hash: params.password })
             .pipe(map((res: any) => {
               // login successful if there's a jwt token in the response
               if (res && res.access_token) {
