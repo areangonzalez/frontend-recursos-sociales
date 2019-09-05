2e87c90529951dad145c515c425e24ece9a5e25c
areangonzalez <areangonzalez@gmail.com>
2019-09-05 12:25:26 -0300

    TG-126 #Done TEST - se da de baja una prestacion

--- e2e/src/app-dar-baja-prestacion.e2e-spec.ts
+++ e2e/src/app-dar-baja-prestacion.e2e-spec.ts
@@ -2,7 +2,7 @@ import { by, browser, element } from "protractor";
 import { AppCabeceraPage } from "./page-object/app.cabecera-po";
 import { AppLoginPage } from "./page-object/app.login-po";
 
-describe('Buscador de persona',  () => {
+describe('Dar baja una prestación',  () => {
   let login: AppLoginPage;
   let cabecera: AppCabeceraPage;
   // Inicio de session para realizar tareas de testing
@@ -42,6 +42,64 @@ describe('Buscador de persona',  () => {
       expect(resultado.length).toBe(4);
     });
   });
+
+  it('abrir modal para dar de baja la prestación', () => {
+    // componente de listado de recursos
+    let listaRecursoComp = element(by.tagName('lista-recurso'));
+    // busco en el listado para clickear el formulario de baja
+    listaRecursoComp.element(by.tagName('table tbody')).all(by.tagName('tr')).then(function(filas) {
+      // clickeo el boton de baja a la primera prestación
+      filas[0].element(by.css('td modal-baja-component')).click();
+    });
+    // espero a que se abra el modal
+    browser.waitForAngular();
+
+    let modalBajaContent = element(by.css('div.modal-body'));
+
+    expect(modalBajaContent.element(by.id('fecha_baja')).isPresent()).toBe(true);
+  });
+
+  it('Completo formulario', () => {
+    // contenido del modal
+    let modalBajaContent = element(by.css('div.modal-body'));
+
+    // agrego fecha de baja
+    modalBajaContent.element(by.id('fecha_baja')).sendKeys('10/10/2014');
+    // agrego descripción de baja
+    modalBajaContent.element(by.id('descripcion')).sendKeys('Se realiza baja por falta de presupuesto');
+
+    element(by.css('div.modal-footer button.btn-success')).click();
+    browser.waitForAngular();
+
+    expect(element(by.css('div.modal-body p')).getText()).toEqual('¿Esta seguro que desea dar de baja la prestación?');
+  });
+
+  it('Confirmo baja', () => {
+    // confirmo la baja
+    element(by.css('div.modal-footer button#confirmacion_baja')).click();
+    // espero que se de baja
+    browser.waitForAngular();
+
+    let alertComp = element(by.tagName('mensajes-alert'));
+
+    expect(alertComp.element(by.css('div.mensaje-alert-body p')).getText()).toEqual('Se ha confirmado la baja.');
+
+    alertComp.element(by.css('button.btn-success')).click();
+    browser.waitForAngular();
+  });
+
+  it('confirmo la prestacion dada de baja', () => {
+    // componente de listado de recursos
+    let listaRecursoComp = element(by.tagName('lista-recurso'));
+    // verifico si la prestacion se dio de baja
+    listaRecursoComp.element(by.tagName('table tbody')).all(by.tagName('tr')).then(function(filas) {
+      // verifico si el boton de baja esta presente
+      expect(filas[0].element(by.css('td modal-baja-component')).isPresent()).toBeFalsy();
+    });
+
+  });
+
+
 
   // Cierro la session una vez terminadas las tareas de testing
   afterAll(() => {
