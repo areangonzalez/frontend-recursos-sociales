7fd6280668e5dd0f85d34596f7043944e6ac36b9
areangonzalez <areangonzalez@gmail.com>
2020-04-13 19:54:51 -0300

    TG-395 #Closed se diseña el formulario de grupo familiar

--- src/app/shared/formulario/grupo-familiar/form-grupo-familiar.component.ts
+++ src/app/shared/formulario/grupo-familiar/form-grupo-familiar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { UtilService } from 'src/app/core/utils';
 
 @Component({
   selector: 'shared-form-grupo-familiar',
@@ -7,10 +8,15 @@ import { FormGroup } from '@angular/forms';
 })
 export class FormGrupoFamiliarComponent implements OnInit {
   @Input("formGrupoFamiliar") public grupoFamiliar: FormGroup;
+  @Input("submitted") public submitted: boolean;
 
-  constructor(){}
+  constructor(private _util: UtilService){}
 
   ngOnInit() {
+  }
+
+  public formatFechaNacimiento(obj:any){
+    this.grupoFamiliar.controls.fecha_nacimiento.setValue(this._util.formatearFecha(obj.day, obj.month, obj.year, "yyyy-MM-dd"));
   }
 
 }
