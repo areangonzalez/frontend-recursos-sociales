a26d42ef02d4d7afce1666deccf20cc7ad855af8
areangonzalez <areangonzalez@gmail.com>
2020-04-27 17:41:35 -0300

    TG-427 #Closed se aplican estilos al submenu de reportes

--- src/app/reporte/menu-list/menu-lista.component.ts
+++ src/app/reporte/menu-list/menu-lista.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 @Component({
     selector: 'reporte-menu-lista',
     templateUrl: './menu-lista.component.html',
+    styleUrls: ['./menu-lista.component.sass']
 })
 export class MenuListaComponent implements OnInit {
 
