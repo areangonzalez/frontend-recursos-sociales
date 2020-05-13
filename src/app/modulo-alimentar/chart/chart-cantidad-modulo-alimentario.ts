f1574e8c31e4994ccb965a725ced48d67a685376
areangonzalez <areangonzalez@gmail.com>
2020-05-13 13:23:45 -0300

    TG-436 #Closed se actualiza el grafico

--- src/app/modulo-alimentar/chart/chart-cantidad-modulo-alimentario.ts
+++ src/app/modulo-alimentar/chart/chart-cantidad-modulo-alimentario.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
+//@ts-ignore
+import * as am4core from "@amcharts/amcharts4/core";
+//@ts-ignore
+import * as am4charts from "@amcharts/amcharts4/charts"
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'ma-chart-cantidadad-modulo-alimentar',
@@ -7,60 +13,26 @@ import { Chart } from 'chart.js';
 })
 export class ChartCantidadModuloAlimentarComponent implements OnInit {
   @Input("listadoLocalidadesMa") public listadoLocalidadesMa: any;
-  public chart:any;
   public colorsGrafico = ['#FC4A1A', '#F7B733', '#4ABDAC', "#0375B4", "#007849", "#C0B283", "#E37222", "#4717F6", "#A239CA", "#CAEBF2", "#E9C893"];
+
+
 
   constructor(){}
 
   ngOnInit() {
-    this.mostrarGrafico();
     this.configurarGrafico(this.listadoLocalidadesMa);
   }
 
   private configurarGrafico(listado: any) {
-    console.log(listado);
-    let datos: any = {labels: [], cantidad_modulos: []};
-    for (let i = 0; i < listado.length; i++) {
-      this.chart.data.labels.push(listado[i].localidad);
-      this.chart.data.datasets[0].data.push(listado[i].modulo_cantidad);
-        /* datos.labels.push(listado[i].localidad + " modulo: " + listado[i].modulo_cantidad);
-        datos.cantidad_modulos.push(listado[i].modulo_cantidad); */
-    }
-    /* this.chart.data.labels = datos.labels;
-    this.chart.data.datasets[0].data = datos.cantidad_modulos; */
+    let chart = am4core.create("chardiv", am4charts.PieChart3D);
+    // creo los labels del grafico
+    chart.legend = new am4charts.Legend();
+    // ingreso los datos
+    chart.data = listado;
 
-    this.chart.update();
-  }
-
-  private mostrarGrafico(){
-    this.chart = new Chart('cantidad-modulo', {
-      type: 'pie',
-      data: {
-        labels: [],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: this.colorsGrafico,
-            fill: false
-          }
-        ]
-      },
-      options: {
-        tooltips: {
-          mode: 'point'
-        },
-        animation: {
-          duration: 500,
-          easing: "easeOutQuart"
-        },
-        responsive: true,
-        legend: {
-          display: true,
-          position: 'left'
-        },
-      }
-    });
-
+    let series = chart.series.push(new am4charts.PieSeries3D());
+    series.dataFields.value = "modulo_cantidad";
+    series.dataFields.category = "localidad";
   }
 
 }
