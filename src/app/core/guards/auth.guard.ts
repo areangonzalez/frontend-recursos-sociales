723405e43b2abb2399461abfcca73b3fceb42779
areangonzalez <areangonzalez@gmail.com>
2020-12-09 10:52:51 -0300

    TG-450 #Closed se diseño adaptativo para distintas pantallas

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { JwtService } from '../services';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private _jwtService: JwtService) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      /* let currentUser = this._jwtService.getToken();
+      const isLoggedIn = currentUser && currentUser.datosToken && currentUser.datosToken.token;
+      // verifico si el usuario esta logueado
+      if (isLoggedIn) {
+        // checkeo if la ruta esta restringida por el rol
+        if (route.data.roles && route.data.roles.index) {
+
+        }
+
+
+      } else {
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+      } */
+
         if (localStorage.getItem('token-rrss')) {
             // logged in so return true
             return true;
         }
-
         // not logged in so redirect to login page with the return url
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
         return false;
