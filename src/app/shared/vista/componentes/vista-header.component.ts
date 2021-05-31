34f2a19e4d5b04bc1a6f25e77335fcf45fe9eace
areangonzalez <areangonzalez@gmail.com>
2021-05-31 10:41:44 -0300

    se arregla error de vista en acreditacion de prestacion

--- src/app/shared/vista/componentes/vista-header.component.ts
+++ src/app/shared/vista/componentes/vista-header.component.ts
@@ -13,4 +13,17 @@ export class VistaHeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  public datosCuota(recurso: any) {
+    let cuota: any = {};
+    cuota["cuota"] = recurso.cuota;
+    cuota["cant_cuota"] = parseInt(recurso.cant_cuota) + 1;
+    cuota["monto_total"] = recurso.monto;
+    cuota["monto_mensual"] = (parseFloat(recurso.monto_mensual) == 0) ? recurso.monto : recurso.monto_mensual;
+    cuota["monto_total_acreditado"] = recurso.monto_total_acreditado;
+    cuota["monto_resto"] = recurso.monto_resto;
+    cuota["monto"] = (parseFloat(recurso.monto_resto) < parseFloat(recurso.monto_mensual)) ? recurso.monto_resto : recurso.monto_mensual;
+
+    return cuota;
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: '/apimock'
+  // baseUrl: '/apimock'
+  baseUrl: 'http://prestaciones-sociales.local/api'
 };
 
 /*
